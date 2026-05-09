460201adfcf142fb3c52ccee52a1e4ecd0f2798f
Abner Daniel <abner.casagrande@escola.pr.gov.br>
2026-05-09 16:56:02 -0300

    ajustando.
    Added charset definition to support accented characters.

--- conexao.php
+++ conexao.php
@@ -1,13 +1,18 @@
 <?php
-/*variaveis de conexão com o mysql*/
-$servidor ="localhost";
-$usuario = "root";
-$senha = "";
-$banco ="crud_produtos";
+// Variáveis de conexão com o MySQL
+$servidor = "localhost";
+$usuario  = "root";
+$senha    = "";
+$banco    = "crud_produtos";
 
-$conexao = mysqli_connect($servidor,$usuario,$senha,$banco);
+// Conectar ao banco
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 
-if(!$conexao){
-    die("Erro de conexão:.mysqli_conect_error()");
-    }
+// Verificar conexão
+if (!$conexao) {
+    die("Erro de conexão: " . mysqli_connect_error());
+}
+
+// Definir charset para aceitar acentos
+mysqli_set_charset($conexao, "utf8");
 ?>
